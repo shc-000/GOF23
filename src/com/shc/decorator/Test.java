dedb82ccf545a267f299a2277e7b5c76e057d726
shc <modelfrog@163.com>
2018-04-06 11:09:59 +0800

    test passwd

---
+++ src/com/shc/decorator/Test.java
@@ -0,0 +1,28 @@
+/**   
+ * Copyright © 2018 eSunny Info. Tech Ltd. All rights reserved.  yoyosys
+ * 
+ * 功能描述：
+ * @Package: com.shc.decorator 
+ * @author: shaohaichao   
+ * @date: 2018年4月6日 上午11:08:42 
+ */
+package com.shc.decorator;
+
+ /**   
+* Copyright: Copyright (c) 2018 yoyosys
+* 
+* @ClassName: Test.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: shaohaichao
+* @date: 2018年4月6日 上午11:08:42 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2018年4月6日     shaohaichao           v1.0.0               修改原因
+*/
+public class Test {
+
+}
